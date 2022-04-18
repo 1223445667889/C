5479fce9cc33944b76f5369d8224f462da75c048
try to do better <99059814+1223445667889@users.noreply.github.com>
2022-04-18 14:28:34 +0800

    Add files via upload

---
+++ Project4/C4.c
@@ -0,0 +1,7 @@
+﻿已启动生成…
+1>------ 已启动生成: 项目: Project4, 配置: Debug x64 ------
+1>game.c
+1>D:\course\C\Project4\game.c(73,31): warning C4013: “get_mine_count”未定义；假设外部返回 int
+1>Project4.vcxproj -> D:\course\C\Project4\x64\Debug\Project4.exe
+1>已完成生成项目“Project4.vcxproj”的操作。
+========== 生成: 成功 1 个，失败 0 个，最新 0 个，跳过 0 个 ==========
